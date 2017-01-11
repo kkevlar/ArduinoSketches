9ab250579577a0a41a0096174b97db71ea30be9d
flipturnapps <kellar.kevin@gmail.com>
2017-01-11 22:32:06 +0000

    Finished background counter

--- src/workspace/binary-led-counter/binary_led_counter.ino
+++ src/workspace/binary-led-counter/binary_led_counter.ino
@@ -1,22 +1,37 @@
 #include "Arduino.h"
 const int SWITCH_PIN = 2;
+const int FIRST_LED_PIN = 3;
+const int NO_LEDS = 5;
 const int DOWN_THRESHOLD_MILLIS = 100;
 int pressCount = 0;
-
+long changeStartTime = 0;
+int currState = 0;
 void setup()
 {
   pinMode(SWITCH_PIN, INPUT);
 }
 
+void switchPressIncrementSpotlight()
+{
+	long time = millis();
+	int newState = readSwitchState();
+	if (newState == currState)
+		return;
+	if(currState == 1 &&
+			changeStartTime - time >
+			DOWN_THRESHOLD_MILLIS)
+		pressCount++;
+
+	changeStartTime = time;
+	currState = newState;
+}
 
 int readSwitchState()
 {
 	int init = digitalRead(SWITCH_PIN);
-	delay(10);
-	int second = digitalRead(SWITCH_PIN);
-	if (init == second)
-	{
+	//delay(10);
+	//int second = digitalRead(SWITCH_PIN);
+	//if (init == second)
       return init;
-	}
-	return 0;
+	//return 0;
 }
