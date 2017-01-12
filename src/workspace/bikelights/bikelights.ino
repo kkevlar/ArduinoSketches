cd8a5291299679094bbf17a561706fe2deae7b05
flipturnapps <kellar.kevin@gmail.com>
2017-01-12 19:30:15 +0000

    Began work on bikelights

--- src/workspace/bikelights/bikelights.ino
+++ src/workspace/bikelights/bikelights.ino
@@ -2,7 +2,7 @@
 
 const int SWITCH_PIN = 2;
 const int FIRST_LED_PIN = 5;
-const int NO_LEDS = 2;
+const int NO_LEDS = 5;
 const int DOWN_THRESHOLD_MILLIS = 25;
 
 int pressCount = 0;
@@ -25,15 +25,12 @@ void loop()
 	switchPressIncrementSpotlight();
 	if(loopCount != pressCount)
 	{
-		int state1 = LOW;
-		int state2 = LOW;
-		if(pressCount % 2 == 0)
-			state1 = HIGH;
-		else
-			state2 = HIGH;
-
-		digitalWrite(FIRST_LED_PIN, state1);
-		digitalWrite((FIRST_LED_PIN+1), state2);
+		if(pressCount % 5 == 1)
+			for(int i = 0; i < NO_LEDS; i++)
+					digitalWrite((FIRST_LED_PIN + i), LOW);
+		if (pressCount % 5 == 0)
+			for(int i = 0; i < NO_LEDS; i++)
+								digitalWrite((FIRST_LED_PIN + i), HIGH);
 		loopCount = pressCount;
 	}
 }
