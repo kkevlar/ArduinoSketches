62f1b212b435159c00e4dc52639a5f0919f3c529
Kevin Kellar <kellar.kevin@gmail.com>
2017-01-17 08:51:14 -0800

    Finished state2

--- src/workspace/bikelights/bikelights.ino
+++ src/workspace/bikelights/bikelights.ino
@@ -10,6 +10,8 @@ long changeStartTime = 0;
 int currState = 0;
 int loopCount = -1;
 boolean firstLoopPass = true;
+long state2FlashTime = 0;
+boolean state2FlashState = false;
 void setup()
 {
 	pinMode(SWITCH_PIN, INPUT);
@@ -23,34 +25,48 @@ void loop()
 	if(loopCount != pressCount)
 	{
 		firstLoopPass = true;
-    loopCount = pressCount;
+		loopCount = pressCount;
 	}
- int mod = pressCount % 5;
- if(firstLoopPass = true && mod == 0)
-  for(int i = 0; i < NO_LEDS; i++)
-    digitalWrite((FIRST_LED_PIN + i), LOW);
- if(firstLoopPass = true && mod == 1)
-  for(int i = 0; i < NO_LEDS; i++)
-    digitalWrite((FIRST_LED_PIN + i), HIGH);
-   
-  firstLoopPass = false;
+	int mod = pressCount % 5;
+	if(firstLoopPass = true && mod == 0)
+		for(int i = 0; i < NO_LEDS; i++)
+			digitalWrite((FIRST_LED_PIN + i), LOW);
+	if(firstLoopPass = true && mod == 1)
+		for(int i = 0; i < NO_LEDS; i++)
+			digitalWrite((FIRST_LED_PIN + i), HIGH);
+	if(mod == 2 && millis() - state2FlashTime > 1000 && state2FlashState == false)
+	{
+		for(int i = 0; i < NO_LEDS; i++)
+			digitalWrite((FIRST_LED_PIN + i), HIGH);
+		state2FlashTime = millis();
+		state2FlashState = true;
+	}
+	if(mod == 2 && millis() - state2FlashTime > 1000 && state2FlashState == true)
+	{
+		for(int i = 0; i < NO_LEDS; i++)
+			digitalWrite((FIRST_LED_PIN + i), LOW);
+		state2FlashTime = millis();
+		state2FlashState = false;
+	}
+
+	firstLoopPass = false;
 }
 
 void switchPressIncrementSpotlight()
 {
-  long time = millis();
-  int newState = readSwitchState();
-  if (newState == currState)
-    return;
-  if(currState == HIGH &&
-      time - changeStartTime >
-      DOWN_THRESHOLD_MILLIS)
-    pressCount++;
-   if(time - changeStartTime <
-      DOWN_THRESHOLD_MILLIS)
-      return;
-  changeStartTime = time;
-  currState = newState;
+	long time = millis();
+	int newState = readSwitchState();
+	if (newState == currState)
+		return;
+	if(currState == HIGH &&
+			time - changeStartTime >
+	DOWN_THRESHOLD_MILLIS)
+		pressCount++;
+	if(time - changeStartTime <
+			DOWN_THRESHOLD_MILLIS)
+		return;
+	changeStartTime = time;
+	currState = newState;
 }
 
 
