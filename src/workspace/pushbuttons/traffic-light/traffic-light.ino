e26a9d690b82c3c1eea5b811bfd24050ddfce911
Kevin Kellar <kellar.kevin@gmail.com>
2017-01-19 09:12:49 -0800

    Traffic lights ready to test

--- src/workspace/pushbuttons/traffic-light/traffic-light.ino
+++ src/workspace/pushbuttons/traffic-light/traffic-light.ino
@@ -3,36 +3,150 @@
  */
 const int SWITCH_PIN = 2;               //Sets the input pin
 const int DOWN_THRESHOLD_MILLIS = 100;  //Sets how long the pushbutton needs to be down
-                                        //	for the program to trigger a press
 
-int pressCount = 0;                     //Counts total # of presses
+const int TL_RED_PIN = 5;
+const int TL_YLW_PIN = 6;
+const int TL_GRN_PIN = 7;
+const int PS_RED_PIN = 9;
+const int PS_GRN_PIN = 10;
+
+const int DUR_INIT_STATE = 5000;
+const int DUR_YELLOW_STATE = 5000;
+const int DUR_RED_STATE = 2000;
+const int DUR_PED_STATE = 5000;
+const int DUR_FLASH_STATE = 5000;
+const int DUR_FINAL_STATE = 2000;
+
+const byte STATE_GRN = 5;
+const byte STATE_YLW = 1;
+const byte STATE_RED = 2;
+const byte STATE_PED = 3;
+const byte STATE_FLASH = 4;
+
+const int DURATIONS[] = {DUR_INIT_STATE,
+		DUR_YELLOW_STATE,
+		DUR_RED_STATE,
+		DUR_PED_STATE,
+		DUR_FLASH_STATE,
+		DUR_FINAL_STATE}
+const byte STATES[] = {STATE_YLW,
+		STATE_RED,
+		STATE_PED,
+		STATE_FLASH,
+		STATE_RED,
+		STATE_GRN,}
+}
+
 long changeStartTime = 0;               //Time since last button state change
 int currState = 0;                      //Current state of the pushbutton
-int loopCount = -1;                     //The loop() function's way of knowing of a
-                                        //	change in the count of button presses
-boolean fLP = false;                    //Tells whether this is loop()'s first
-                                        //	execution (FirstLoopPass)
+boolean button_pressed = false;
+boolean startSequence = false;                   
+long startTime = 0;
+byte lightsState = 0;
+boolean isFlashStateLit = false;
 
 void setup()
 {
 	pinMode(SWITCH_PIN, INPUT);         //The switch is the only input in the program
-}
+	pinMode(TL_RED_PIN, OUTPUT);
+	pinMode(TL_YLW_PIN, OUTPUT);
+	pinMode(TL_GRN_PIN, OUTPUT);
+	pinMode(PS_RED_PIN, OUTPUT);
+	pinMode(PS_GRN_PIN, OUTPUT);
+	setState(STATE_GRN);
+} 
+
 void loop()
 {
 	switchPressIncrementSpotlight();    //Runs the function to check for button changes
-	if(loopCount != pressCount)         //If the number of counts has changed....
+	if(button_pressed && !startSequence)         //If the number of counts has changed....
 	{
-		fLP = true;                     //Tell rest of loop that its first time in state
-		loopCount = pressCount;         //Account for the change so this if block is
-		                                //	only run once per state change
+		loop_has_passed = true;
+		startTime = millis();
 	}
-	
-	fLP = false;                         //Stores that the loop has completed at least once
+	if(!startSequence)
+		return;
+	long time = startTime;
+	byte newState = 0;
+	for(int i = 0; i < sizeof(STATES); i++)
+	{
+		time += DURATIONS[i];
+		if(millis() > time)
+			newState = STATES[i];
+		else
+			break;
+	}
+	if(newState == lightsState && lightsState != STATE_FLASH)
+		return;
+	lightsState = newState;
+	setState(lightsState);	
+	if(lightsState == STATE_GRN)
+	{
+		lightsState = 0;
+		startSequence = false;
+		button_pressed = false;
+		startTime = 0;
+	}
+}
+
+void setState(byte state)
+{
+	if(state == STATE_GRN)
+	{
+		digitalWrite(TL_RED_PIN, LOW);
+		digitalWrite(TL_YLW_PIN, LOW);
+		digitalWrite(TL_GRN_PIN, HIGH);
+		digitalWrite(PS_RED_PIN, HIGH);
+		digitalWrite(PS_GRN_PIN, LOW);
+	}
+	if(state == STATE_YLW)
+	{
+		digitalWrite(TL_RED_PIN, LOW);
+		digitalWrite(TL_YLW_PIN, HIGH);
+		digitalWrite(TL_GRN_PIN, LOW);
+		digitalWrite(PS_RED_PIN, HIGH);
+		digitalWrite(PS_GRN_PIN, LOW);
+	}
+	if(state == STATE_RED)
+	{
+		digitalWrite(TL_RED_PIN, HIGH);
+		digitalWrite(TL_YLW_PIN, LOW);
+		digitalWrite(TL_GRN_PIN, LOW);
+		digitalWrite(PS_RED_PIN, HIGH);
+		digitalWrite(PS_GRN_PIN, LOW);
+	}
+	if(state == STATE_PED)
+	{
+		digitalWrite(TL_RED_PIN, HIGH);
+		digitalWrite(TL_YLW_PIN, LOW);
+		digitalWrite(TL_GRN_PIN, LOW);
+		digitalWrite(PS_RED_PIN, LOW);
+		digitalWrite(PS_GRN_PIN, HIGH);
+	}
+	if(state == STATE_FLASH)
+	{
+		digitalWrite(TL_RED_PIN, HIGH);
+		digitalWrite(TL_YLW_PIN, LOW);
+		digitalWrite(TL_GRN_PIN, LOW);
+		digitalWrite(PS_RED_PIN, LOW);
+		float div = 500;
+		int count = (int) (millis()/div);
+		boolean isLit = false;
+		if(count % 2 = 1)
+			isLit = true;
+		if(isLit != isFlashStateLit)
+		{
+			isFlashStateLit = isLit;
+			if(isLit)
+				digitalWrite(PS_GREEN_PIN, HIGH);
+			else
+				digitalWrite(PS_GREEN_PIN, LOW);
+		}
+	}
 }
 
 void switchPressIncrementSpotlight()
 {
-
 	long time = millis();
 	int newState = readSwitchState();   //Runs the function to read the switches state
 	if (newState == currState)          //If the state hasn't changed, leave the funtion
@@ -40,13 +154,12 @@ void switchPressIncrementSpotlight()
 	if(currState == HIGH &&
 			time - changeStartTime >
 	DOWN_THRESHOLD_MILLIS)              //If the switch was down and was held long enough...
-		pressCount++;                   //Then increment the number of presses
+		button_pressed = true;          //Store that the button was pressed
 	if(time - changeStartTime <
 			DOWN_THRESHOLD_MILLIS)      //If the switch changed state to quick....
 		return;                         //Leave the function
 	changeStartTime = time;             //Store when the switch changed state
 	currState = newState;               //Store that the switch did change state
-
 }
 
 
