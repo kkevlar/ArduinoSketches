58ef5d77e5b740c494a6348fcc481c5d5bc1f7cc
Kevin Kellar <kellar.kevin@gmail.com>
2017-01-20 11:17:57 -0800

    Fixed the 4button bug

--- src/workspace/newmorse/newmorse.ino
+++ src/workspace/newmorse/newmorse.ino
@@ -1,14 +1,14 @@
 
-const int DWN_TIME = 100;
+const int DWN_TIME = 50;
 
 const byte COLOR_RED = 0;
 const byte COLOR_GREEN = 1;
 const byte COLOR_YELLOW = 2;
 const byte COLOR_BLUE = 3;
-const byte[] COLOR_SEQUENCE = {COLOR_RED,COLOR_GREEN,COLOR_YELLOW,COLOR_BLUE};
+const byte COLOR_SEQUENCE[] = {COLOR_RED,COLOR_GREEN,COLOR_YELLOW,COLOR_BLUE};
 
-const byte[] PIN_BUTTONS = {11,10,9,8};
-const byte[] PIN_LEDS = {5,4,3,2};
+const byte PIN_BUTTONS[] = {11,10,9,8};
+const byte PIN_LEDS[] = {5,4,3,2};
 
 long buttonChangeTimes[sizeof(PIN_BUTTONS)];
 int buttonStates[sizeof(PIN_BUTTONS)];
@@ -18,7 +18,7 @@ boolean shouldChangeLeds;
 void setup()
 {
 	for(int i = 0; i < sizeof(PIN_BUTTONS); i++)
-		pinMode(PIN_BUTTONS[i], INPUT_PULLUP);
+		pinMode(PIN_BUTTONS[i], INPUT);
 	for(int i = 0; i < sizeof(PIN_LEDS); i++)
 		pinMode(PIN_LEDS[i], OUTPUT);
 	pinMode(13,OUTPUT);
@@ -58,6 +58,7 @@ void switchPressTester()
 	for(int i = 0; i < sizeof(PIN_BUTTONS); i++)
 	{
 		int newState = digitalRead(PIN_BUTTONS[i]);
+   
 		/*
 		if(redButtonState == LOW && time - redChangeTime > 2000)
 		{
@@ -67,16 +68,17 @@ void switchPressTester()
 			return;
 		}
 		*/
-		if (newState == buttonStates[i])
-			return;
-
-		if(buttonStates[i] == HIGH && time - buttonChageTimes[i] > DWN_TIME)
+   
+		if(newState == buttonStates[i])
+			continue;
+    
+		if(buttonStates[i] == HIGH && time - buttonChangeTimes[i] > DWN_TIME)
 			buttonPressed(COLOR_SEQUENCE[i]);
 
-		if(time - buttonChageTimes[i] < DWN_TIME)
-			return;
+		if(time - buttonChangeTimes[i] < DWN_TIME)
+			continue;
 
-		buttonChageTimes[i] = time;
+		buttonChangeTimes[i] = time;
 		buttonStates[i] = newState;
 	}
 }
