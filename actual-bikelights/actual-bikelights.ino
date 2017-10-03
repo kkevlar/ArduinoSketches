3b0694caa08ac7047a8ffc495f4bb3de80340ecc
Kevin Kellar <kellar.kevin@gmail.com>
2017-10-03 10:59:18 -0700

    stage1 of actual bikelights should be ready to go

--- actual-bikelights/actual-bikelights.ino
+++ actual-bikelights/actual-bikelights.ino
@@ -1,6 +1,7 @@
-const int PIN_RED = 5;
-const int PIN_GRN = 6;
-const int PIN_BLU = 9;
+#define PIN_RED 5
+#define PIN_GRN 6
+#define PIN_BLU 9
+#define PIN_BUTTON 3
 
 const byte dim_curve[] = {
     0, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
@@ -21,24 +22,50 @@ const byte dim_curve[] = {
     193, 196, 200, 203, 207, 211, 214, 218, 222, 226, 230, 234, 238, 242, 248, 255,
 };
 
+int state = 5;
+long offset = 0;
+int frozenHue = -1;
+int colors[3];
+int buttonState = LOW;
+long bStateChangeMillis = 0;
+bool allowStateIncrease = true;
 
 void setup()
 {
-
     pinMode(PIN_RED, OUTPUT);
     pinMode(PIN_GRN, OUTPUT);
     pinMode(PIN_BLU, OUTPUT);
+    pinMode(PIN_BUTTON, INPUT_PULLUP);
 
     Serial.begin(9600);
 }
 
+int getHueFromMillis()
+{
+    return (millis()-offset/10)%360
+}
+
+void setLedsByHSV(int hue, int sat, int val)
+{
+   getRGB(hue, sat, val, colors);
+   int red = dim_curve[(colors[0])];
+   int green = dim_curve[(colors[1])];
+   int blue = dim_curve[(colors[2])];
+   analogWrite(PIN_RED, red);
+   analogWrite(PIN_GRN, green);
+   analogWrite(PIN_BLU, blue); 
+}
+
+void setLedsByHue(int hue)
+{
+    setLedsByHSV(hue, 255, 255);
+}
+
 void loop()
 {
-    boolean dont = false;
-
-   Serial.print("Joystick 1's value is ");
-   Serial.println(analogRead(A0));
-    for (int x = 0; x < 3600; x++) {
+    /*
+    for (int x = 0; x < 3600; x++) 
+    {
         int colors[3];
 
         getRGB(int(x / 10), 255, 255, colors);
@@ -51,8 +78,51 @@ void loop()
         
         delay(5);
     }
+    */
 
-   
+    if(state == 0)
+        setLedsByHue(getHueFromMillis());
+    if (state != 1)
+        frozenHue = -1;
+    else
+        if (frozenHue == -1)
+            frozenHue == getHueFromMillis();
+        else        
+            setLedsByHue(frozenHue);
+    if (state == 2)
+        if ((millis() / 200) % 2 == 0)
+            setLedsByHSV(frozenHue, 255, 255);
+        else
+            setLedsByHSV(frozenHue, 255, 0);
+    if (state == 3)
+        setLedsByHSV(0,0,255);
+    if (state == 4)
+        if ((millis() / 200) % 2 == 0)
+            setLedsByHSV(frozenHue, 0, 255);
+        else
+            setLedsByHSV(frozenHue, 0, 0);
+    if (state == 5)
+        setLedsByHSV(0,0,0);
+
+    int nState = digitalRead(PIN_BUTTON);
+    if (nState != buttonState)
+    {
+        buttonState = nState;
+        bStateChangeMillis = millis();
+    }
+    if(millis() - bStateChangeMillis > 100 &&
+        buttonState == LOW &&
+        && allowStateIncrease)
+    {
+        state = (state+1)%6;
+        allowStateIncrease = false;
+    }
+    if(millis() - bStateChangeMillis > 100 &&
+        buttonState == HIGH)
+    {
+         allowStateIncrease = true;
+    }
+
 }
 
 void getRGB(int hue, int sat, int val, int colors[3])
